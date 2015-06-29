65020e8faac99c8c5ec08438e5df51ea88700c3c
hinderson <mattias@hinderson.com>
2015-06-29 18:40:57 +0200

    Remove deprecated favicon module

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var favicon = require('serve-favicon');
 var fs = require('fs');
 var path = require('path');
-var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
