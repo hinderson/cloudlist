635137c2a94e60da4dce1aa3743cd3a8082742c2
hinderson <mattias@hinderson.com>
2015-10-07 23:06:45 +0200

    Add more functional methods to collections factory

--- client/js/data/collection.js
+++ client/js/data/collection.js
@@ -63,10 +63,41 @@ var sortCollection = function (items, key, reverse) {
 	return order;
 };
 
+var getItem = function (id) {
+	return getCollection()[id];
+};
+
+var getItemPosition = function (id) {
+	return getCollectionOrder().indexOf(id);
+};
+
+var getNextItem = function (currentIndex) {
+	return getCollectionOrder()[currentIndex + 1] || getFirstItem();
+};
+
+var getPreviousItem = function (currentIndex) {
+	return getCollectionOrder()[currentIndex - 1] || getLastItem();
+};
+
+var getFirstItem = function ( ) {
+	return getCollectionOrder()[0];
+};
+
+var getLastItem = function ( ) {
+	var collectionLength = getCollectionOrder().length;
+	return getCollectionOrder()[collectionLength - 1];
+};
+
 // Export interface
 module.exports = {
 	setCollection: setCollection,
 	getCollection: getCollection,
 	getCollectionOrder: getCollectionOrder,
-	sortCollection: sortCollection
+	sortCollection: sortCollection,
+	getItem: getItem,
+	getItemPosition: getItemPosition,
+	getNextItem: getNextItem,
+	getPreviousItem: getPreviousItem,
+	getFirstItem: getFirstItem,
+	getLastItem: getLastItem
 };
